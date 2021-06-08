4cc60a180f5d9887f028b72304bf71c4cd451dc9
Jack Gitter <gitter.j@husky.neu.edu>
2021-06-07 21:43:57 -0400

    updated code but still not working

--- test.ino
+++ test.ino
@@ -2,16 +2,13 @@
 #include <HashMap.h>
 #include <IRremote.h>
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 6;
 const byte HASHSIZE = 8;
-int RECV_PIN = 9;
-
-IRrecv irrecv(RECV_PIN);
-decode_results results; 
+const byte IR_RECEIVE_PIN = 2;
 
 
 enum Person {
-  Jack, Chris, Mason, Will, Evan, Nick, Declan, Mehdi 
+  Jack, Chris, Mason, Will, Evan, Nick, Declan, Mehdi
 };
 
 enum Stat {
@@ -21,23 +18,23 @@ enum Stat {
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-HashType<Person,int> hashRawArray[HASHSIZE];
-HashMap<Person,int> wins = HashMap<Person,int>(hashRawArray, HASHSIZE);
-HashMap<Person,int> avgCups = HashMap<Person,int>(hashRawArray, HASHSIZE);
-HashMap<Person,int> gamesPlayed = HashMap<Person,int>(hashRawArray, HASHSIZE);
+HashType<Person, int> hashRawArray[HASHSIZE];
+HashMap<Person, int> wins = HashMap<Person, int>(hashRawArray, HASHSIZE);
+HashMap<Person, int> avgCups = HashMap<Person, int>(hashRawArray, HASHSIZE);
+HashMap<Person, int> gamesPlayed = HashMap<Person, int>(hashRawArray, HASHSIZE);
 
 Person currPlayer = Jack;
-Stat currStat = Wins; 
+Stat currStat = Wins;
 
 
 Person findNextPlayer(Person currPlayer) {
-  
-  switch(currPlayer){
+
+  switch (currPlayer) {
     case Jack:
       return Chris;
     case Chris:
       return Mason;
-    case Mason: 
+    case Mason:
       return Will;
     case Will:
       return Evan;
@@ -52,12 +49,12 @@ Person findNextPlayer(Person currPlayer) {
     default:
       return Jack;
   }
-  
+
 }
 
 String evalEnum2(Stat s) {
-  
-  switch(s) {
+
+  switch (s) {
     case Wins:
       return "Wins: ";
     case AverageCups:
@@ -65,12 +62,12 @@ String evalEnum2(Stat s) {
     default:
       return "No Stat Selected";
   }
-  
+
 }
 
 String evalEnum(Person p) {
-  
-  switch(p) {
+
+  switch (p) {
     case 0:
       return "Jack";
     case 1:
@@ -83,19 +80,19 @@ String evalEnum(Person p) {
       return "Evan";
     case 5:
       return "Nick";
-    case 6: 
+    case 6:
       return "Declan";
-    case 7: 
+    case 7:
       return "Mehdi";
     default:
       return "err";
   }
-  
+
 }
 
 
 void setup() {
-  
+
   wins[0](Jack, 0);
   wins[1](Chris, 0);
   wins[2](Mason, 0);
@@ -104,7 +101,6 @@ void setup() {
   wins[5](Nick, 0);
   wins[6](Declan, 0);
   wins[7](Mehdi, 0);
-
 
   avgCups[0](Jack, 0);
   avgCups[1](Chris, 0);
@@ -123,26 +119,26 @@ void setup() {
   gamesPlayed[5](Nick, 0);
   gamesPlayed[6](Declan, 0);
   gamesPlayed[7](Mehdi, 0);
+  
 
   Serial.begin(9600);
-  Serial.print("output");
-  irrecv.enableIRIn();
-  
+  Serial.print("IR Receive test");
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+
   Person goat = Jack;
   lcd.begin(16, 2);
   lcd.print(evalEnum2(currStat));
   lcd.setCursor(0, 1);
   lcd.print(evalEnum(Jack));
-    
-  
-  
+
+
+
 }
 
 void loop() {
 
-    if (irrecv.decode(&results)) {
-      Serial.println(results.value, HEX);
-      Serial.println("hello");
-      irrecv.resume();
-    }
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.command, HEX);
+    IrReceiver.resume();
+  }
 }
